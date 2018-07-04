c2bdceb596c30792d2feec5983ea1723a8693045
Vivek Bhookya <v.bhookya98@gmail.com>
2018-07-04 16:09:55 -0500

    proper flow of windows

--- linkManager.js
+++ linkManager.js
@@ -6,6 +6,7 @@
 // o When opening SpeedDial, we check first if a state is saved
 // + If a state is saved, we populate the list and display popup2.html with the numbers
 // + If no state is saved, we display popup1.html so users can specify a list
+//
 // o Add a way for users to change the state they are in
 // o create arrays of organizations with their phone numbers per state ugh
 
@@ -18,18 +19,28 @@
 $(document).ready(function () {
   // document.getElementById('TEST4').innerHTML='state'; // this should run after we get STATE from storage
   // populate(); // fill list with phone numbers
-
-  $("#submit").click(save); // save the state to storage
   init();
+  $("#submit").click(save); // save the state to storage
 });
 
 // Function init()
 // Grab STATE from storage and replace 'test values' with the state
 function init() {
   chrome.storage.sync.get('STATE', function(returnedObj) {
+    if (!returnedObj.STATE && window.location.href !== 'chrome-extension://cjbjlcdbnfckcbodkefdlihlbahaokig/popup1.html') {
+      window.open('popup1.html', "_self");
+      // alert(window.location.href);
+      // populate_list(returnedObj.STATE);
+    }
+
     document.getElementById('TEST4').innerHTML = returnedObj.STATE;
+
+
   });
 
+}
+
+function populate_list(){
 
 }
 
