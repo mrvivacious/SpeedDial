9be9509773ed6ec2118c15178c551af318f5cde3
Vivek Bhookya <v.bhookya98@gmail.com>
2018-07-04 16:35:02 -0500

    We can change our state now

--- linkManager.js
+++ linkManager.js
@@ -3,10 +3,7 @@
 // @author Vivek Bhookya
 
 // TODO::
-// o When opening SpeedDial, we check first if a state is saved
-// + If a state is saved, we populate the list and display popup2.html with the numbers
-// + If no state is saved, we display popup1.html so users can specify a list
-//
+
 // o Add a way for users to change the state they are in
 // o create arrays of organizations with their phone numbers per state ugh
 
@@ -15,12 +12,22 @@
 
 // Finished
 // √ Save state submitted when users click "submit" into chrome.storage.sync
+// √ When opening SpeedDial, we check first if a state is saved
+// + If a state is saved, we populate the list and display popup2.html with the numbers
+// + If no state is saved, we display popup1.html so users can specify a list
+
+ var YWCA = {
+   'AL': [' 2053224878'],
+   'IL': ['2173440721', '8882932080'],
+   'VA': ['8046126126'],
+};
 
 $(document).ready(function () {
   // document.getElementById('TEST4').innerHTML='state'; // this should run after we get STATE from storage
   // populate(); // fill list with phone numbers
   init();
   $("#submit").click(save); // save the state to storage
+  $("#change").click(remove);
 });
 
 // Function init()
@@ -29,12 +36,10 @@ function init() {
   chrome.storage.sync.get('STATE', function(returnedObj) {
     if (!returnedObj.STATE && window.location.href !== 'chrome-extension://cjbjlcdbnfckcbodkefdlihlbahaokig/popup1.html') {
       window.open('popup1.html', "_self");
-      // alert(window.location.href);
-      // populate_list(returnedObj.STATE);
     }
 
     document.getElementById('TEST4').innerHTML = returnedObj.STATE;
-
+     populate_list(returnedObj.STATE);
 
   });
 
@@ -49,4 +54,19 @@ function populate_list(){
 function save() {
   let state = document.getElementById('state')[0].value;
   chrome.storage.sync.set( {STATE: state}, function() {});
+}
+
+function remove() {
+  chrome.storage.sync.remove('STATE', function() {});
+}
+
+
+// Function addLink()
+// Create a new list item when clicking on the "Add" button
+function addLink() {
+  // "Declare" a li object
+  let li = document.createElement("li");
+
+  // let t = phoneNumber;
+  // li.appendChild(t);
 }
